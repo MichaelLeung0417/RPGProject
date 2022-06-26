3fb1703263672b0776dce6846b56bad526aef6d7
SevenBoii <assy2166@gmail.com>
2022-06-26 19:26:14 +0800

    login, logout and register done

---
+++ main.ts
@@ -0,0 +1,74 @@
+import { triggerAsyncId } from "async_hooks";
+import express from "express";
+import expressSession from "express-session";
+import fs from "fs";
+
+const main = express();
+
+main.use(
+  expressSession({
+    secret: "qwpoejqpwoejqpeoj",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+main.use(express.urlencoded());
+
+main.use(express.static("public"));
+
+main.post("/login", (req, res) => {
+  if (req.body.username === "tester" && req.body.password === "123") {
+    req.session["isLogin"] = true;
+  }
+});
+
+const isLogin = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if ((req.session["isLogin"] = true)) {
+    next();
+  } else {
+    res.send("wrong password");
+  }
+};
+
+main.get("/login", isLogin, (req, res) => {
+  res.redirect("/index.html");
+});
+
+main.post("/logout", (req, res) => {
+  req.session["isAdmin"] = false;
+  res.redirect("/");
+});
+
+main.post("/register", async (req, res) => {
+  let users;
+  try {
+    users = JSON.parse(await fs.promises.readFile("users.json", "utf8"));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+    return;
+  }
+
+  for (const user of users) {
+    if (user.username.trim() === req.body.username.trim()) {
+      res.redirect("/?error=重覆username");
+      return;
+    }
+  }
+
+  users.push({
+    username: req.body.username.trim(),
+    password: req.body.password.trim(),
+  });
+
+  await fs.promises.writeFile("users.json", JSON.stringify(users));
+
+  res.redirect("/");
+});
+
+main.listen(8000);
