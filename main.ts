14215ca7d0d07032a11bcf0b30819c95202fb27d
SevenBoii <assy2166@gmail.com>
2022-06-27 10:15:39 +0800

    prettier update ver2.0

--- main.ts
+++ main.ts
@@ -1,78 +1,78 @@
-import express from "express";
-import expressSession from "express-session";
-import fs from "fs";
+import express from 'express'
+import expressSession from 'express-session'
+import fs from 'fs'
 
-const main = express();
+const main = express()
 
 main.use(
-  expressSession({
-    secret: "qwpoejqpwoejqpeoj",
-    saveUninitialized: true,
-    resave: true,
-  })
-);
+	expressSession({
+		secret: 'qwpoejqpwoejqpeoj',
+		saveUninitialized: true,
+		resave: true
+	})
+)
 
-main.use(express.urlencoded());
+main.use(express.urlencoded())
 
-main.use(express.static("public"));
+main.use(express.static('public'))
 
-main.get("/", (req, res) => {
-  res.redirect("login.html");
-});
+main.get('/', (req, res) => {
+	res.redirect('login.html')
+})
 
-main.post("/login", (req, res) => {
-  if (req.body.username === "tester" && req.body.password === "123") {
-    req.session["isUser"] = true;
-  }
-  res.send("done");
-});
+main.post('/login', (req, res) => {
+	if (req.body.username === 'tester' && req.body.password === '123') {
+		req.session['isUser'] = true
+	}
+	res.send('done')
+})
 
 const isLogin = (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
+	req: express.Request,
+	res: express.Response,
+	next: express.NextFunction
 ) => {
-  if ((req.session["isUser"] = true)) {
-    next();
-  } else {
-    res.send("wrong password");
-  }
-};
+	if ((req.session['isUser'] = true)) {
+		next()
+	} else {
+		res.send('wrong password')
+	}
+}
 
-main.post("/logout", (req, res) => {
-  req.session["isUser"] = false;
-  res.redirect("/");
-});
+main.post('/logout', (req, res) => {
+	req.session['isUser'] = false
+	res.redirect('/')
+})
 
-main.get("/game", isLogin, (req, res) => {
-  res.redirect("/index.html");
-});
+main.get('/game', isLogin, (req, res) => {
+	res.redirect('/index.html')
+})
 
-main.post("/register", async (req, res) => {
-  let users;
-  try {
-    users = JSON.parse(await fs.promises.readFile("users.json", "utf8"));
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-    return;
-  }
+main.post('/register', async (req, res) => {
+	let users
+	try {
+		users = JSON.parse(await fs.promises.readFile('users.json', 'utf8'))
+	} catch (err) {
+		console.error(err)
+		res.status(500).send('Internal Server Error')
+		return
+	}
 
-  for (const user of users) {
-    if (user.username.trim() === req.body.username.trim()) {
-      res.redirect("/?error=重覆username");
-      return;
-    }
-  }
+	for (const user of users) {
+		if (user.username.trim() === req.body.username.trim()) {
+			res.redirect('/?error=重覆username')
+			return
+		}
+	}
 
-  users.push({
-    username: req.body.username.trim(),
-    password: req.body.password.trim(),
-  });
+	users.push({
+		username: req.body.username.trim(),
+		password: req.body.password.trim()
+	})
 
-  await fs.promises.writeFile("users.json", JSON.stringify(users));
+	await fs.promises.writeFile('users.json', JSON.stringify(users))
 
-  res.redirect("/");
-});
+	res.redirect('/')
+})
 
-main.listen(8000);
+main.listen(8000)
--- public/index.js
+++ public/index.js
@@ -1,117 +1,117 @@
-const scale = 2;
-const width = 16;
-const height = 18;
-const scaled_width = scale * width;
-const scaled_height = scale * height;
-const cycle_loop = [0, 1, 0, 2];
-const facing_down = 0;
-const facing_up = 1;
-const facing_left = 2;
-const facing_right = 3;
-const frame_limit = 12;
-const movement_speed = 2;
+const scale = 2
+const width = 16
+const height = 18
+const scaled_width = scale * width
+const scaled_height = scale * height
+const cycle_loop = [0, 1, 0, 2]
+const facing_down = 0
+const facing_up = 1
+const facing_left = 2
+const facing_right = 3
+const frame_limit = 12
+const movement_speed = 2
 
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
-let keyPresses = {};
-let currentDirection = facing_down;
-let currentLoopIndex = 0;
-let frameCount = 0;
-let positionX = 0;
-let positionY = 0;
-let img = new Image();
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+let keyPresses = {}
+let currentDirection = facing_down
+let currentLoopIndex = 0
+let frameCount = 0
+let positionX = 0
+let positionY = 0
+let img = new Image()
 
-window.addEventListener("keydown", keyDownListener);
+window.addEventListener('keydown', keyDownListener)
 function keyDownListener(event) {
-  keyPresses[event.key] = true;
+	keyPresses[event.key] = true
 }
 
-window.addEventListener("keyup", keyUpListener);
+window.addEventListener('keyup', keyUpListener)
 function keyUpListener(event) {
-  keyPresses[event.key] = false;
+	keyPresses[event.key] = false
 }
 
 function loadImage() {
-  img.src =
-    "https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png";
-  img.onload = function () {
-    window.requestAnimationFrame(gameLoop);
-  };
+	img.src =
+		'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png'
+	img.onload = function () {
+		window.requestAnimationFrame(gameLoop)
+	}
 }
 
 function drawFrame(frameX, frameY, canvasX, canvasY) {
-  ctx.drawImage(
-    img,
-    frameX * width,
-    frameY * height,
-    width,
-    height,
-    canvasX,
-    canvasY,
-    scaled_width,
-    scaled_height
-  );
+	ctx.drawImage(
+		img,
+		frameX * width,
+		frameY * height,
+		width,
+		height,
+		canvasX,
+		canvasY,
+		scaled_width,
+		scaled_height
+	)
 }
 
-loadImage();
+loadImage()
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  let hasMoved = false;
+	let hasMoved = false
 
-  if (keyPresses.w) {
-    moveCharacter(0, -movement_speed, facing_up);
-    hasMoved = true;
-  } else if (keyPresses.s) {
-    moveCharacter(0, movement_speed, facing_down);
-    hasMoved = true;
-  }
+	if (keyPresses.w) {
+		moveCharacter(0, -movement_speed, facing_up)
+		hasMoved = true
+	} else if (keyPresses.s) {
+		moveCharacter(0, movement_speed, facing_down)
+		hasMoved = true
+	}
 
-  if (keyPresses.a) {
-    moveCharacter(-movement_speed, 0, facing_left);
-    hasMoved = true;
-  } else if (keyPresses.d) {
-    moveCharacter(movement_speed, 0, facing_right);
-    hasMoved = true;
-  }
+	if (keyPresses.a) {
+		moveCharacter(-movement_speed, 0, facing_left)
+		hasMoved = true
+	} else if (keyPresses.d) {
+		moveCharacter(movement_speed, 0, facing_right)
+		hasMoved = true
+	}
 
-  if (hasMoved) {
-    frameCount++;
-    if (frameCount >= frame_limit) {
-      frameCount = 0;
-      currentLoopIndex++;
-      if (currentLoopIndex >= cycle_loop.length) {
-        currentLoopIndex = 0;
-      }
-    }
-  }
+	if (hasMoved) {
+		frameCount++
+		if (frameCount >= frame_limit) {
+			frameCount = 0
+			currentLoopIndex++
+			if (currentLoopIndex >= cycle_loop.length) {
+				currentLoopIndex = 0
+			}
+		}
+	}
 
-  if (!hasMoved) {
-    currentLoopIndex = 0;
-  }
+	if (!hasMoved) {
+		currentLoopIndex = 0
+	}
 
-  drawFrame(
-    cycle_loop[currentLoopIndex],
-    currentDirection,
-    positionX,
-    positionY
-  );
-  window.requestAnimationFrame(gameLoop);
+	drawFrame(
+		cycle_loop[currentLoopIndex],
+		currentDirection,
+		positionX,
+		positionY
+	)
+	window.requestAnimationFrame(gameLoop)
 }
 
 function moveCharacter(deltaX, deltaY, direction) {
-  if (
-    positionX + deltaX > 0 &&
-    positionX + scaled_width + deltaX < canvas.width
-  ) {
-    positionX += deltaX;
-  }
-  if (
-    positionY + deltaY > 0 &&
-    positionY + scaled_height + deltaY < canvas.height
-  ) {
-    positionY += deltaY;
-  }
-  currentDirection = direction;
+	if (
+		positionX + deltaX > 0 &&
+		positionX + scaled_width + deltaX < canvas.width
+	) {
+		positionX += deltaX
+	}
+	if (
+		positionY + deltaY > 0 &&
+		positionY + scaled_height + deltaY < canvas.height
+	) {
+		positionY += deltaY
+	}
+	currentDirection = direction
 }
