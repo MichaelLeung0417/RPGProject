60cda84580559c8e51b13dee409c0465c5196ee0
kenBF <ken456789123@gmail.com>
2022-07-04 12:25:06 +0800

    database init basic complete

--- localhost.session.sql
+++ localhost.session.sql
@@ -1,1 +1,13 @@
+CREATE TABLE users
+(
+  id         SERIAL    NOT NULL,
+  username   VARCHAR(255)  UNIQUE,
+  password   VARCHAR(255),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO students (username, password, created_at, updated_at) VALUES ('admin',123, NOW(), NOW());
+
 SELECT * FROM users
