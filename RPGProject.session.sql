0824f82850ca2812cf1566083a5e3248fdd7e158
SevenBoii <assy2166@gmail.com>
2022-07-06 15:09:38 +0800

    game.ts update

--- RPGProject.session.sql
+++ RPGProject.session.sql
@@ -1,1 +1,8 @@
-SELECT * FROM accounts;
+CREATE TABLE message(
+    id SERIAL primary key,
+    text VARCHAR(255),
+    created_at TIMESTAMP with time zone,
+    updated_at TIMESTAMP with time zone
+)
+
+DROP TABLE students;
