8aaeef593329babdef90f536d902d7cdc5a32f1e
SevenBoii <assy2166@gmail.com>
2022-07-07 16:06:14 +0800

    player movement backend logic done

--- gameData/room.ts
+++ gameData/room.ts
@@ -1,10 +1,23 @@
 import { Character } from './player'
 import { Monster } from './monster'
-import { Map } from './map'
 
 export default class Gameroom {
 	private onlinePlayers: Character[] = []
 	private existedBugs: Monster[] = []
+	private boardColumns: number = 20
+	private boardRows: number = 20
+
+	constructor(
+		onlinePlayers: Character[],
+		existedBugs: Monster[],
+		boardColumns: number,
+		boardRows: number
+	) {
+		this.onlinePlayers = onlinePlayers
+		this.existedBugs = existedBugs
+		this.boardColumns = boardColumns
+		this.boardRows = boardRows
+	}
 
 	AddPlayer(player: Character) {
 		this.onlinePlayers.push(player)
@@ -21,10 +34,16 @@ export default class Gameroom {
 		}[] = []
 
 		for (const player of this.onlinePlayers) {
-			playerInMap.push({
-				x: player.getPlayerData().position.x,
-				y: player.getPlayerData().position.y
-			})
+			while (
+				player.getPlayerData().position.x < this.boardColumns &&
+				player.getPlayerData().position.y < this.boardRows
+			) {
+				playerInMap.push({
+					x: player.getPlayerData().position.x,
+					y: player.getPlayerData().position.y
+				})
+			}
 		}
+		return playerInMap
 	}
 }
