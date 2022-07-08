1d07160ebd129e10aac7611a7cc0d3e40d02dc32
SevenBoii <assy2166@gmail.com>
2022-07-08 10:22:00 +0800

    game.ts updated

--- gameData/game.ts
+++ gameData/game.ts
@@ -1,21 +1,16 @@
-// import { Character } from './player'
+export class Map {
+	private boardColumns: number = 20
+	private boardRows: number = 20
 
-// export class ClientHandler {
-// 	// private boardColumns: number = 20
-// 	// private boardRows: number = 20
-// 	private playerNames: string[] = []
-// 	// private map: Map
+	constructor(boardColumns: number, boardRows: number) {
+		this.boardColumns = boardColumns
+		this.boardRows = boardRows
+	}
 
-// 	getPlayers() {
-// 		this.playerNames.push(Character.name)
-// 		console.log(this.playerNames)
-// 	}
-// }
-
-// // private getAllPlayers() {
-// //     let playersReturn = []
-// //       for (const player of room.players) {
-// //         playersReturn.push(character.getPlayerData())
-// //       }
-// //     return playersReturn
-// //   }
+	getMap() {
+		return {
+			boardColumns: this.boardColumns,
+			boardRows: this.boardRows
+		}
+	}
+}
