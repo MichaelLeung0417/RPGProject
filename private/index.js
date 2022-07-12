2eb516ed4d824f0db3339573523e88096ab742a0
SevenBoii <assy2166@gmail.com>
2022-07-12 18:16:58 +0800

    index.js debugged

--- private/index.js
+++ private/index.js
@@ -9,7 +9,7 @@ let bugsLocation = {}
 
 function setup() {
 	canvas = createCanvas(680, 680)
-	img = loadImage('./floor1.png')
+	img = loadImage('./floor2.png')
 	canvas.parent('canvas')
 	columns = floor(width / gridSize)
 	rows = floor(height / gridSize)
@@ -83,17 +83,17 @@ function keydown(e) {
 	socket.emit('keydown', e.keyCode)
 }
 
-//battling player
-let battlePlayer = document.createElement('div')
-battlePlayer.classList.add('player')
-document.querySelector('#BattleScene1').appendChild(battlePlayer)
+// //battling player
+// let battlePlayer = document.createElement('div')
+// battlePlayer.classList.add('player')
+// document.querySelector('#BattleScene1').appendChild(battlePlayer)
 
-//battling player fir ball
-let fireball = document.createElement('div')
-fireball.classList.add('fireball')
-document.querySelector('#BattleScene1').appendChild(fireball)
+// //battling player fir ball
+// let fireball = document.createElement('div')
+// fireball.classList.add('fireball')
+// document.querySelector('#BattleScene1').appendChild(fireball)
 
-//Alex
-let Alex = document.createElement('div')
-Alex.classList.add('Alex')
-document.querySelector('#BattleScene1').appendChild(Alex)
+// //Alex
+// let Alex = document.createElement('div')
+// Alex.classList.add('Alex')
+// document.querySelector('#BattleScene1').appendChild(Alex)
