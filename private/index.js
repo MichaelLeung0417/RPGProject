a19c7a1abf2750b90077bacda7bf03b7acf9b6cf
kenBF <ken456789123@gmail.com>
2022-07-12 15:00:25 +0800

    battle background image fine

--- private/index.js
+++ private/index.js
@@ -1,3 +1,5 @@
+const { url } = require("inspector");
+
 // const data = require('../map/QQQ')
 let img;
 let board
@@ -20,6 +22,9 @@ function setup() {
 		keydown(e)
 	})
 }
+
+//import battle image
+//document.querySelector('#BattleScene1').style.backgroundImage = loadImage('./classroom2.png')
 
 //typing detection
 let typing = false
