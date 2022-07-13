8ce6d68ac3da27601133807a8c2c1a54477473d5
LAPTOP-R59J1MVB\Michael's PC <auytfff123@gmail.com>
2022-07-13 14:30:22 +0800

    add new background

--- private/status.js
+++ private/status.js
@@ -3,7 +3,9 @@ socket.on('playerHp', (data) => {
 	document.getElementById('playerHp').innerHTML = data
 })
 
+
 //get player level
 socket.on('playerLevel', (data) => {
 	document.getElementById('playerLevel').innerHTML = data
 })
+
